79d5859e68f78112558dd953c8e63ae81d5f0013
Michael Lopez <michael@childpovertyactionlab.org>
2021-11-08 11:00:52 -0600

    check update

---
+++ R/firearms.R
@@ -0,0 +1,9 @@
+#### List of available firearm types in NIBRS Incidents #####
+firearms <- c("Handgun",
+              "Firearm (Type Not Stated)",
+              "Assault Weapon",
+              "Unknown Type Gun",
+              "Rifle",
+              "Other/Unknown Gun",
+              "Shotgun",
+              "Other Gun")
