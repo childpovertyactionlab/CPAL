3dec4bab03e79a39bb706ca22ee7d31b5544f267
Michael Lopez <michael@childpovertyactionlab.org>
2021-12-02 07:01:13 -0600

    new functions

---
+++ R/dpd_vgc.R
@@ -0,0 +1,10 @@
+#' List of violent gun crime NIBRS incidents used in Dallas Police Department Police Incidents data.
+#'
+#' A dataset containing fix NIBRS incident types
+#'
+#' @format A data frame with 6 rows and 1 variables:
+#' \describe{
+#'   \item{firearm}{a gun type}
+#'   ...
+#' }
+"dpd_vgc"
